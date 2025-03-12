cd7cffc3ff3c740edfdb1e0c57001f46ce899f08
santdiaz <santi.diaz2001@gmail.com>
2025-03-12 12:23:52 -0300

    Authservces

--- src/app/sesion/register/register.component.ts
+++ src/app/sesion/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { profile } from 'src/app/Interfaces/models';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Importar FormBuilder y Validators
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +9,43 @@ import { profile } from 'src/app/Interfaces/models';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-
+  // Definir la variable para perfiles
   profi = profile;
 
-  constructor() { }
+  // Crear el formulario de registro
+  registerForm: FormGroup;
 
-  ngOnInit(): void {
+  // Inyectamos AuthService y FormBuilder
+  constructor(private authService: AuthService, private fb: FormBuilder) {
+    // Inicializamos el formulario
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      profile: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  // Método para enviar el formulario
+  onRegister(): void {
+    if (this.registerForm.valid) {
+      const { firstName, lastName, username, password, profile } = this.registerForm.value;
+
+      // Usamos el servicio de AuthService para registrar el usuario
+      this.authService.register(username, password).subscribe({
+        next: (response) => {
+          console.log('Usuario registrado con éxito', response);
+          // Aquí puedes redirigir al usuario a la página de login o de inicio
+        },
+        error: (error) => {
+          console.error('Error al registrar usuario', error);
+        }
+      });
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
